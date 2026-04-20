de99fc7dcc7050aae86941fa86b22e40419de23f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-19 20:30:14 -0500

    feat: create commong files and config them

--- todo-list/Commons/ToDoExtensions.swift
+++ todo-list/Commons/ToDoExtensions.swift
@@ -5,4 +5,23 @@
 //  Created by Camilo on 4/19/26.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    public func cornerRadius(
+        _ radius: CGFloat,
+        corners: UIRectCorner
+    ) -> some View {
+        clipShape(
+        ToDoRoundedCorner(
+            radius: radius,
+            corners: corners
+        ))
+    }
+    
+    public func transparentScrolling() -> some View {
+        return onAppear {
+            UITextView.appearance().backgroundColor = .clear
+        }
+    }
+}
