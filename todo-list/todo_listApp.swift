f016b3ae289de3dd2939d0019d9ac3ad4051ecdf
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-19 20:03:25 -0500

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Camilo on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
