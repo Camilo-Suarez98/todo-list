f171cccd96b4f1a2c1b99bce9bb0495667716941
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-19 20:29:41 -0500

    feat: config common files

---
+++ todo-list/Commons/ToDoExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoExtensions.swift
+//  todo-list
+//
+//  Created by Camilo on 4/19/26.
+//
+
+import Foundation
---
+++ todo-list/Commons/ToDoRoundedCorner.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoRoundedCorner.swift
+//  todo-list
+//
+//  Created by Camilo on 4/19/26.
+//
+
+import SwiftUI
+
+struct ToDoRoundedCorner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoRoundedCorner()
+}
