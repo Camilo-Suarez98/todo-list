4af4f60b949329ee98285c6119f5391b31129c3c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-20 20:29:36 -0500

    feat: config text input field

--- todo-list/Commons/ToDoTextInput.swift
+++ todo-list/Commons/ToDoTextInput.swift
@@ -7,12 +7,66 @@
 
 import SwiftUI
 
-struct ToDoTextInput: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ToDoTextInput: View {
+    public var label: String?
+    public let placeholder: String
+    public var variant: Variant?
+    
+    @Binding public var text: String
+    
+    public enum Variant {
+        case textField
+        case textEditor
     }
-}
-
-#Preview {
-    ToDoTextInput()
+    
+    init(
+        _ placeholder: String,
+        text: Binding<String>,
+        label: String? = nil,
+        variant: Variant? = .textField
+    ) {
+        self.label = label
+        self.placeholder = placeholder
+        self._text = text
+        self.variant = variant
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            if let label = label {
+                Text(label)
+                    .font(.title3)
+            }
+            
+            switch variant {
+            case .textField:
+                TextField(placeholder, text: $text)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(UIColor.tertiarySystemBackground))
+                    .cornerRadius(10)
+            case .textEditor:
+                ZStack(alignment: .topLeading) {
+                    if text.isEmpty {
+                        Text(placeholder)
+                            .font(.body)
+                            .foregroundStyle(Color(UIColor.tertiaryLabel))
+                            .padding(.top)
+                            .animation(nil, value: text)
+                    }
+                    TextEditor(text: $text)
+                        .font(.body)
+                        .padding(.horizontal, -4)
+                        .padding(.top, 8)
+                }
+                .padding(.horizontal)
+                .frame(height: 76)
+                .transparentScrolling()
+                .background(Color(UIColor.tertiarySystemBackground))
+                .cornerRadius(10)
+            default:
+                EmptyView()
+            }
+        }
+    }
 }
