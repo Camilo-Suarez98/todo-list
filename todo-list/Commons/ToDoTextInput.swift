00656afe0e84bb5f1595c98cafe2467929a027bf
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-20 20:29:13 -0500

    feat: create text input field

---
+++ todo-list/Commons/ToDoTextInput.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoTextInput.swift
+//  todo-list
+//
+//  Created by Camilo on 4/20/26.
+//
+
+import SwiftUI
+
+struct ToDoTextInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoTextInput()
+}
