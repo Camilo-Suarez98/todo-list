21d2a59e4af66edd3243e20491f793416a2f5f1d
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-22 21:17:18 -0500

    feat: create to do add view

---
+++ todo-list/Views/ToDoAdd/ToDoAddView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoAddView.swift
+//  todo-list
+//
+//  Created by Camilo on 4/22/26.
+//
+
+import SwiftUI
+
+struct ToDoAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoAddView()
+}
