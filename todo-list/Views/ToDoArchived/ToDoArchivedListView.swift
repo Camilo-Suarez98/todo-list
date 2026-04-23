88fb5084961e542a5e1633294641e199db672046
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-22 21:15:36 -0500

    feat: create archived list view

---
+++ todo-list/Views/ToDoArchived/ToDoArchivedListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoArchivedListView.swift
+//  todo-list
+//
+//  Created by Camilo on 4/22/26.
+//
+
+import SwiftUI
+
+struct ToDoArchivedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoArchivedListView()
+}
