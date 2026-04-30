667c2c256362c707bf6c3fbaaa6ee869a5c1f4c5
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-04-29 21:31:08 -0500

    feat: config persistance manager

--- todo-list/Commons/ToDoPersistenceManager.swift
+++ todo-list/Commons/ToDoPersistenceManager.swift
@@ -5,4 +5,21 @@
 //  Created by Camilo on 4/29/26.
 //
 
-import Foundation
+import CoreData
+
+final class ToDoPersistenceManager {
+    static let shared = ToDoPersistenceManager()
+    
+    let container: NSPersistentContainer
+    
+    private init() {
+        container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Error cargando datos: \(error)")
+            } else {
+                print("Carga de datos exitosa")
+            }
+        }
+    }
+}
