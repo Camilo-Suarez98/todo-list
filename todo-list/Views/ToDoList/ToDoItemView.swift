96f6b02410d6aa97d113837c9b1088e7cc85bed3
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-05-09 19:07:40 -0500

    feat: create component to do item view

---
+++ todo-list/Views/ToDoList/ToDoItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItemView.swift
+//  todo-list
+//
+//  Created by Camilo on 5/9/26.
+//
+
+import SwiftUI
+
+struct ToDoItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItemView()
+}
